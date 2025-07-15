f9faf5143de5641c3ae517f16c9c4fc0f9d70e0e
caior139 <67523502+caior139@users.noreply.github.com>
2025-07-14 22:06:58 -0300

    Add files via upload

--- degree_of_saturation_algorithm.py
+++ degree_of_saturation_algorithm.py
@@ -1,69 +1,33 @@
 import time
 
 def degree_of_saturation_algorithm(grafo):
-
     inicio = time.process_time()
-    cores = {}
-    grau = {}
+    cores = {vertice: None for vertice in grafo}
+    grau = {vertice: len(grafo[vertice]) for vertice in grafo}
+    cores_vizinhos = {vertice: set() for vertice in grafo}
+
     lista_cores = [0]
     proxima_cor = 1
 
-    for vertice in grafo:
-        cores[vertice] = None
-        grau[vertice] = len(grafo[vertice])
-
-    lista_grau = []
-    for item in grau.items():
-        lista_grau.append(item)
-    lista_grau.sort(key=lambda x: x[1], reverse=True)
-
-    primeiro_vertice = lista_grau[0][0]
+    primeiro_vertice = max(grafo, key=lambda vertice: grau[vertice])
     cores[primeiro_vertice] = 0
+    for vizinho in grafo[primeiro_vertice]:
+        cores_vizinhos[vizinho].add(0)
 
-    while True:
-        vizinhos_coloridos = {}
-        for vertice in grafo:
-            if cores[vertice] is not None:
-                continue
-            count = 0
-            for cor in lista_cores:
-                for vizinho in grafo[vertice]:
-                    if cores[vizinho] == cor:
-                        count += 1
-                        break
-            vizinhos_coloridos[vertice] = count
+    nao_coloridos = set(grafo) - {primeiro_vertice}
 
-        if not vizinhos_coloridos:
-            break
-
-        max_saturacao = -1
-        vertice_para_colorir = None
-        for vertice in vizinhos_coloridos:
-            sat = vizinhos_coloridos[vertice]
-            if (vertice_para_colorir is None
-                or sat > max_saturacao
-                or (sat == max_saturacao and grau[vertice] > grau[vertice_para_colorir])
-            ):
-                max_saturacao = sat
-                vertice_para_colorir = vertice
-
-        cor_escolhida = -1
-        for cor in lista_cores:
-            pode_usar = True
-            for vizinho in grafo[vertice_para_colorir]:
-                if cores[vizinho] == cor:
-                    pode_usar = False
-                    break
-            if pode_usar:
-                cor_escolhida = cor
-                break
-
-        if cor_escolhida == -1:
-            cor_escolhida = proxima_cor
-            lista_cores.append(proxima_cor)
+    while nao_coloridos:
+        vertice_escolhido = max(nao_coloridos, key=lambda vertice: (len(cores_vizinhos[vertice]), grau[vertice]))
+        usadas = cores_vizinhos[vertice_escolhido]
+        cor = next((c for c in lista_cores if c not in usadas), None)
+        if cor is None:
+            cor = proxima_cor
+            lista_cores.append(cor)
             proxima_cor += 1
-
-        cores[vertice_para_colorir] = cor_escolhida
+        cores[vertice_escolhido] = cor
+        for vizinho in grafo[vertice_escolhido]:
+            cores_vizinhos[vizinho].add(cor)
+        nao_coloridos.remove(vertice_escolhido)
 
     fim = time.process_time()
-    return [cores,fim-inicio]
+    return [cores, fim - inicio]
